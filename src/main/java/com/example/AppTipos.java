fda9f58eb0d97860183dc2c052f53251d9e35cd2
Fernando Ferret <fernandoferret@hotmail.com>
2023-08-02 21:49:28 +0000

    Teste3

---
+++ src/main/java/com/example/AppTipos.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class AppTipos {
+ 
+    public static void main(String[] args) {
+
+        // Tipos númericos - Aula 3
+
+        int idade = 50;
+        byte qualquer = 127;
+        double dobro = 10.0;
+        float varOps = (float) dobro;
+        char serie = 'A';
+        boolean buliana = false;
+  
+        System.out.println("\n");
+        System.out.println("A idade é " + idade);
+        System.out.println("qualquer é " + qualquer);
+        System.out.println("varOps é " + varOps);
+        System.out.println("A série é " + serie);
+        System.out.println("Variável buliana é " + buliana);
+        System.out.println("\n");
+
+    }
+
+}
