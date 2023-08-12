b1e0d83a8129cc054342025ef25f0207bbcb4551
Fernando Ferret <fernandoferret@hotmail.com>
2023-08-12 18:41:09 +0000

    Teste6

---
+++ src/main/java/com/example/AppClasses.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class AppClasses {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.setRenda(20000);
+        cliente.setSexo('M');
+        cliente.setAnoNascimento(1950);
+        System.out.println("----------");
+        System.out.println("Renda: " + cliente.getRenda());
+        System.out.println("Sexo: " + cliente.getSexo());
+        System.out.println("Ano: " + cliente.getAnoNascimento());
+        System.out.println("----------");
+    }
+}
---
+++ src/main/java/com/example/Cliente.java
@@ -0,0 +1,38 @@
+package com.example;
+
+public class Cliente {
+    private double renda;
+    private char sexo;
+    private int anoNascimento;
+
+    public double getRenda(){
+        return renda;
+    }
+
+    public void setRenda (double renda){
+        if (renda >= 0)
+            this.renda = renda;
+        else
+            System.out.println("A renda não pode ter um valor negativo!");
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        if (sexo=='M'||sexo=='F')
+            this.sexo = sexo;
+        else
+            System.out.println("Erro na definição do sexo");
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+}
